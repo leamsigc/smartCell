ec1a1acc8d4dc8e60fb87a8e3b4a8abf3929d756
leamsigc <leamsigc@hotmail.com>
2018-03-08 16:50:25 -0500

    create the hero and the card in mobile

--- frontendSmartCell/src/js/app.js
+++ frontendSmartCell/src/js/app.js
@@ -1,16 +1,16 @@
-window.addEventListener('DOMContentLoaded',function(){
+window.addEventListener('DOMContentLoaded', function () {
     console.log('content loaded ....');
 
-   const content = document.querySelector('.main_wrapper'),
-         menuToggle = document.querySelector('.menu_toggle');
+    const menu = document.querySelector('nav'),
+        menuToggle = document.querySelector('.menu_toggle');
 
 
-    menuToggle.addEventListener('click',() => {
+    menuToggle.addEventListener('click', () => {
         console.log('click');
-        content.classList.toggle('menu_on');
+        menu.classList.toggle('active');
     });
 
     document.querySelector('.content').addEventListener('click', () => {
-        content.removeClass('menu_on');
+        content.removeClass('active');
     });
 });
