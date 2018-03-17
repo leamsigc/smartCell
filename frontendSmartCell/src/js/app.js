67ae6b6b1c74ae2ade4e10538e1e92845cb1f334
leamsigc <leamsigc@hotmail.com>
2018-03-17 02:49:54 -0400

    add smoot scroll efect and animate css

--- frontendSmartCell/src/js/app.js
+++ frontendSmartCell/src/js/app.js
@@ -9,4 +9,18 @@ window.addEventListener('DOMContentLoaded', function () {
         console.log('click');
         menu.classList.toggle('active');
     });
+    // const linkElements = document.querySelectorAll('nav > a[href ^= "#"]');
+    // console.log(linkElements);
+    // linkElements.forEach(link => {
+    //     console.log(link.getAttribute('href'));
+    //     link.addEventListener('click',scroll);
+
+    // });
+
+    // // scroll function 
+    // function scroll(link){
+    //     let linkAttribute = this.getAttribute('href');
+
+    //     console.log(linkAttribute);
+    // }
 });
